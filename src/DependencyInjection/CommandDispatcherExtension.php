b80cbd89fdfc4cb865a882a8eea8670c24007aa9
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-11-25 11:53:05 +0100

    Fix Event Dispatcher and service configuration

---
+++ src/DependencyInjection/CommandDispatcherExtension.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at : 25/11/2019
+ */
+
+namespace JeckelLab\CommandDispatcherBundle\DependencyInjection;
+
+use Exception;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+/**
+ * Class CommandDispatcherExtension
+ */
+class CommandDispatcherExtension extends Extension
+{
+    /**
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     * @throws Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.xml');
+    }
+}
